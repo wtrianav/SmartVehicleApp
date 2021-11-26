c6affc410dc4f181f8b9f63c6bb079c3d7e2abf2
Leonardo Perdomo <leonardop763@gmail.com>
2021-11-26 00:43:28 -0500

    Se agrega el metodo post para realizar la recuperacion de la contraseña de los clientes

---
+++ Smart-Vehicle-App/src/models/cambio-clave.model.ts
@@ -0,0 +1,33 @@
+import {Model, model, property} from '@loopback/repository';
+
+@model()
+export class CambioClave extends Model {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  clave_actual: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  clave_nueva: string;
+
+
+  constructor(data?: Partial<CambioClave>) {
+    super(data);
+  }
+}
+
+export interface CambioClaveRelations {
+  // describe navigational properties here
+}
+
+export type CambioClaveWithRelations = CambioClave & CambioClaveRelations;
