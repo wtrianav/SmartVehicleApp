54874251ad885a5fe1c67ecb97534b7667b489fd
Alexa <alexasvr@gmail.com>
2021-11-30 20:04:05 -0500

    elaboración de vista de lista de solicitudes del cliente

--- Smart-Vehicle-App/client/src/app/modules/request/request.module.ts
+++ Smart-Vehicle-App/client/src/app/modules/request/request.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { RequestRoutingModule } from './request-routing.module';
 import { ClientRequestComponent } from './client-request/client-request.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { ListRequestComponent } from './list-request/list-request.component';
 
 
 @NgModule({
   declarations: [
-    ClientRequestComponent
+    ClientRequestComponent,
+    ListRequestComponent
   ],
   imports: [
     CommonModule,
