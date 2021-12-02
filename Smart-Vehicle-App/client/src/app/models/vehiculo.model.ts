f095dabbc3ecf55a2d6060c33c6982780940e8a5
Leonardo Perdomo <leonardop763@gmail.com>
2021-12-01 22:05:11 -0500

    Se agrega el componente para cerrar sesion y se modifica el componente del login de la aplicacion, asi mismo se modifican las propiedades de la solicitud en el backend

---
+++ Smart-Vehicle-App/client/src/app/models/vehiculo.model.ts
@@ -0,0 +1,9 @@
+export class Vehiculo {
+    anio_modelo?: string;
+    id?: string;
+    imagen?: string
+    link_video?: string
+    marca?: string;
+    placa?: string;
+    tipo_vehiculo?: string
+}
